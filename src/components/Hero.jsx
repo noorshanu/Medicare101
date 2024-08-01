3f74b8b7f7887382ea9b809785b27dc13f74d443
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-08-01 12:13:02 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ function Hero() {
     <section className="  bg-[#092f88] ">
       <div className=" container-wrapper   overflow-hidden relative">
 
-        <div className=" flex flex-col sm:flex-row justify-between py-4  sm:py-12">
+        <div className=" flex flex-col sm:flex-row justify-between py-4  items-center sm:py-12">
 
           <div className=" w-full sm:w-1/2" data-aos="zoom-in-right" data-aos-duration="1500">
-          <p className=" py-4 text-sm  mt-[10%] font-semibold text-white">Welcome to Medicare101</p>
+          <p className=" py-4 text-sm  mt-[1%] font-semibold text-white">Welcome to Medicare101</p>
             <h1 className="text-[#fff] text-2xl sm:text-4xl font-semibold  mb-1 ">
             Turning 65 Soon ?  <br /> 
             </h1>
@@ -24,6 +24,8 @@ function Hero() {
             We understand that navigating Medicare can be overwhelming, <br /> especially if you're new to it. That's why we're here to help!
             </p>
 
+
+          <p className=" text-white font-bold  mt-4">Join us clicking the link below,  than select  “join with browser” <br /> to access the seminar</p>
            <div className=" w-[135px]">
            <a href="https://humana.zoom.us/j/91914088518?pwd=sH96F3AraLxf4o64UT7PRvxZxshFaJ.1" target="_blank"  rel="noreferrer" className=" bg-[#1e86ff] px-2 py-2 
            text-xl font-semibold text-white rounded-full my-4 
